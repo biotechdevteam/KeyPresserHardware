a2070cf201bcb60909dc08add47a2ee7c6e8ba97
Nkengbeza Derick <nkengbderick@gmail.com>
2024-09-29 15:51:45 +0100

    config

--- next.config.mjs
+++ next.config.mjs
@@ -7,12 +7,6 @@ const nextConfig = {
   images: {
     domains: ["via.placeholder.com"],
   },
-  // Enabling static export
-  output: "export",
-  // Base path is useful if you're deploying to a subpath in a domain
-  basePath: "",
-  // Disable server-side features to allow for static export
-  trailingSlash: true,
 };
 
 export default withNextIntl(nextConfig);

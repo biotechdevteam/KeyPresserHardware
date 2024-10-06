59c58ebe57dcc0e438d38b63ae7be600ecc4ed59
nyochembeng <nyochembengn@gmail.com>
2024-10-06 19:41:43 +0100

    landing page redirect

--- src/app/[locale]/(web)/page.tsx
+++ src/app/[locale]/(web)/page.tsx
@@ -1,12 +1,13 @@
 import { useTranslations } from "next-intl";
 import ComingSoon from "../coming-soon";
+import LandingTemp from "../landing-temp";
 
 export default function HomePage() {
   const t = useTranslations("HomePage");
   return (
     <div>
       <h1 className="text-center text-4xl mt-4">{t("title")}</h1>
-      <ComingSoon />
+      <LandingTemp/>
     </div>
   );
 }

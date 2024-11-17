16b7e56c0eaffe9e66aca6d5a4ed7dea212370ee
Nkengbeza Derick <nkengbderick@gmail.com>
2024-11-17 06:24:15 +0100

    okay

--- src/components/auth/ResetPassword.tsx
+++ src/components/auth/ResetPassword.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useForm } from "react-hook-form";

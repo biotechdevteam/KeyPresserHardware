38b7d5f88028c036d4d2da8a4552e1c25456f28e
Nkengbeza Derick <nkengbderick@gmail.com>
2024-11-19 00:34:01 +0100

    application done

--- src/types/ApplicationSchema.ts
+++ src/types/ApplicationSchema.ts
@@ -2,12 +2,19 @@ import { z } from "zod";
 
 export const ApplicationSchema = z.object({
   user_id: z.string().nonempty("User ID is required."),
+  profile_photo_url: z
+    .string()
+    .url("Must be a url")
+    .nonempty("Profile photo URL is required."),
   motivation_letter: z
     .string()
     .min(10, "Motivation letter must be at least 10 characters."),
   referred_by_member_id: z.string().optional(),
-  specialization_area: z.string().optional(),
-  resume_url: z.string().url("Must be a valid URL.").optional(),
+  specialization_area: z.string().min(2, "Required please"),
+  resume_url: z
+    .string()
+    .url("Must be a url")
+    .nonempty("Profile photo URL is required.")
 });
 
 export type Application = z.infer<typeof ApplicationSchema>;

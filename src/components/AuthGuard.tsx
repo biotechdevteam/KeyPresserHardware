4e1469af740026f44685f250a9fa6dd00c4593b3
Nkengbeza Derick <nkengbderick@gmail.com>
2024-11-21 07:39:40 +0100

    aut guard

--- src/components/AuthGuard.tsx
+++ src/components/AuthGuard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import useAuth from "@/lib/useAuth";
 import Loader from "./loader/Loader";
@@ -11,7 +11,7 @@ interface AuthGuardProps {
 }
 
 const AuthGuard: React.FC<AuthGuardProps> = ({ children, allowedRoles }) => {
-  const { isAuthenticated, user, loading } = useAuth();
+  const { user } = useAuth();
   const router = useRouter();
 
   useEffect(() => {

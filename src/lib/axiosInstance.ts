7c4a451c213c69f8f0ca9d11883330123cabe804
Nkengbeza Derick <nkengbderick@gmail.com>
2024-11-21 07:40:40 +0100

    okay cool permision management

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 // Set baseURL from environment variable
-//const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
-const baseURL = "http://localhost:5000/api";
+const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+//const baseURL = "http://localhost:5000/api";
 
 console.log("Base URL:", baseURL);
 
